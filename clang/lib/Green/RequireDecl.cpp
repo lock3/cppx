1ecae9dae634b26eb2abb190ac324472da30230c
samg31 <samgoodrick@lock3software.com>
2019-11-27 14:59:02 -0500

    Add files for RequireDecl

---
+++ clang/lib/Green/RequireDecl.cpp
@@ -0,0 +1,16 @@
+//=== RequireDecl.cpp - Declarations for Green Nodes ----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (c) Lock3 Software 2019, all rights reserved.
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file defines the RequireDecl interface, which creates clang nodes
+//  out of typed syntax nodes. This is the third phase of Green Semantic
+//  Action.
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/Green/RequireDecl.h"
