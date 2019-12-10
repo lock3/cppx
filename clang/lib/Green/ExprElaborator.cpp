5399a2005293022ffdc3d574b098b7a7cd8655be
samg31 <samgoodrick@lock3software.com>
2019-12-10 14:42:05 -0500

    Elaboration of some binary operators

--- clang/lib/Green/ExprElaborator.cpp
+++ clang/lib/Green/ExprElaborator.cpp
@@ -1,11 +1,13 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Expr.h"
+#include "clang/AST/OperationKinds.h"
 #include "clang/AST/Type.h"
 #include "clang/Basic/SourceLocation.h"
 #include "clang/Lex/Preprocessor.h"
 #include "clang/Sema/Lookup.h"
 #include "clang/Sema/Sema.h"
 #include "llvm/ADT/APSInt.h"
+#include "llvm/ADT/StringMap.h"
 
 #include "clang/Green/ExprElaborator.h"
 #include "clang/Green/GreenSema.h"
@@ -21,11 +23,24 @@ ExprElaborator::ExprElaborator(ASTContext &ClangContext, GreenSema &SemaRef)
   : ClangContext(ClangContext), SemaRef(SemaRef)
 {}
 
+Expr *
+ExprElaborator::elaborateExpr(const Syntax *S, clang::QualType ExplicitType) {
+  if (isa<AtomSyntax>(S))
+    return elaborateAtom(cast<AtomSyntax>(S), ExplicitType);
+  if (isa<CallSyntax>(S))
+    return elaborateCall(cast<CallSyntax>(S));
+
+  assert(false && "Unsupported expression.");
+}
+
 static IntegerLiteral *
 createIntegerLiteral(ASTContext &ClangContext, Token T, QualType IntType,
                      SourceLocation Loc) {
   llvm::APInt Value;
   unsigned Width = 0;
+
+  if (IntType.isNull())
+    IntType = ClangContext.IntTy;
 
   // TODO: support all kinds of integer types.
   if (IntType.isNull() || IntType == ClangContext.AutoDeductTy) {
@@ -107,7 +122,7 @@ createDeclRefExpr(ASTContext &ClangContext, GreenSema &SemaRef, Preprocessor &PP
 }
 
 Expr *
-ExprElaborator::elaborateExpr(const AtomSyntax *S, QualType ExplicitType) {
+ExprElaborator::elaborateAtom(const AtomSyntax *S, QualType ExplicitType) {
   Token T = S->Tok;
 
   switch (T.getKind()) {
@@ -133,6 +148,98 @@ ExprElaborator::elaborateExpr(const AtomSyntax *S, QualType ExplicitType) {
   }
 
   return nullptr;
+}
+
+// Mapping of Green's fused operator strings to clang Opcodes.
+// TODO: Assignment will probably be handled differently than other bin ops?
+const llvm::StringMap<BinaryOperatorKind> BinaryOperators = {
+  {"operator'+'" , BO_Add},
+  {"operator'-'" , BO_Sub},
+  {"operator'*'" , BO_Mul},
+  {"operator'/'" , BO_Div},
+  {"operator'%'" , BO_Rem},
+  {"operator'&'" , BO_And},
+  {"operator'|'" , BO_Or},
+  {"operator'^'" , BO_Xor},
+  {"operator'&&'" , BO_LAnd},
+  {"operator'||'" , BO_LOr},
+  {"operator'=='" , BO_EQ},
+  {"operator'<>'", BO_NE},
+  {"operator'<'", BO_LT},
+  {"operator'>'", BO_GT},
+  {"operator'<='", BO_LE},
+  {"operator'>='", BO_GE},
+};
+
+const llvm::StringMap<BinaryOperatorKind> CompoundAssignOperators = {
+  {"operator'+='" , BO_AddAssign},
+  {"operator'-='" , BO_SubAssign},
+  {"operator'*='" , BO_MulAssign},
+  {"operator'/='" , BO_DivAssign},
+  {"operator'%='" , BO_RemAssign},
+  {"operator'&='" , BO_AndAssign},
+  {"operator'|='" , BO_OrAssign},
+  {"operator'^='" , BO_XorAssign},
+};
+
+Expr *
+ExprElaborator::elaborateCall(const CallSyntax *S) {
+  const AtomSyntax *Callee = cast<AtomSyntax>(S->Callee());
+  std::string Spelling = Callee->Tok.getSpelling();
+
+  // Check if this is a standard binary operator (one that doesn't assign).
+  auto BinOpMapIter = BinaryOperators.find(Spelling);
+  if (BinOpMapIter != BinaryOperators.end()) {
+    return elaborateBinOp(S, BinOpMapIter->second);
+  }
+
+  // Check if this is a compound assignment operator like operator'+='.
+  auto CmpAssnMapIter = CompoundAssignOperators.find(Spelling);
+  if (CmpAssnMapIter != CompoundAssignOperators.end()) {
+    return elaborateCmpAssignOp(S, CmpAssnMapIter->second);
+  }
+
+  llvm::errs() << "Unsupported call.\n";
+  return nullptr;
+}
+
+Expr *
+ExprElaborator::elaborateBinOp(const CallSyntax *S, BinaryOperatorKind Op) {
+  const ListSyntax *ArgList = cast<ListSyntax>(S->Args());
+  const Syntax *LHSSyntax = ArgList->Elems[0];
+  const Syntax *RHSSyntax = ArgList->Elems[1];
+
+  Expr *RHSExpr = elaborateExpr(LHSSyntax, QualType());
+  Expr *LHSExpr = elaborateExpr(RHSSyntax, QualType());
+
+  clang::Sema &ClangSema = SemaRef.getClangSema();
+
+  ExprResult Res = ClangSema.BuildBinOp(/*Scope=*/nullptr, SourceLocation(), Op,
+                                        LHSExpr, RHSExpr);
+  if (Res.isInvalid())
+    return nullptr;
+
+  return Res.get();
+}
+
+Expr *
+ExprElaborator::elaborateCmpAssignOp(const CallSyntax *S,
+                                     BinaryOperatorKind Op) {
+  const ListSyntax *ArgList = cast<ListSyntax>(S->Args());
+  const Syntax *LHSSyntax = ArgList->Elems[0];
+  const Syntax *RHSSyntax = ArgList->Elems[1];
+
+  Expr *RHSExpr = elaborateExpr(LHSSyntax, QualType());
+  Expr *LHSExpr = elaborateExpr(RHSSyntax, QualType());
+
+  clang::Sema &ClangSema = SemaRef.getClangSema();
+
+  ExprResult Res =
+    ClangSema.CreateBuiltinBinOp(SourceLocation(), Op, LHSExpr, RHSExpr);
+  if (Res.isInvalid())
+    return nullptr;
+
+  return Res.get();
 }
 
 } // namespace green
