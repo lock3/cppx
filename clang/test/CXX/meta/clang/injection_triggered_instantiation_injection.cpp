b77f5d03a9b91938a9f6dea575463dd852e6f1ca
Wyatt Childers <wchilders@nearce.com>
2019-12-18 13:07:13 -0500

    Add a couple of important test cases to the test suite

---
+++ clang/test/CXX/meta/clang/injection_triggered_instantiation_injection.cpp
@@ -0,0 +1,24 @@
+template<int x>
+class nested_foo {
+  consteval -> __fragment struct {
+    int get() {
+      return x;
+    }
+  };
+};
+
+class foo {
+  consteval {
+    int i = 0;
+    -> __fragment struct {
+      int unqualid("get_value_", i)() {
+        return nested_foo<i> { }.get();
+      }
+    };
+  };
+};
+
+int main() {
+  foo f;
+  return f.get_value_0();
+}
