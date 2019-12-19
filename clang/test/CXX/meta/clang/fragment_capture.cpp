259f237b06dee2905b057f1c2cd4b30ba6019de6
Wyatt Childers <wchilders@nearce.com>
2019-12-19 10:49:35 -0500

    WIP

--- clang/test/CXX/meta/clang/fragment_capture.cpp
+++ clang/test/CXX/meta/clang/fragment_capture.cpp
@@ -1,5 +1,9 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -freflection -std=c++2a %s
 
+class ClassWithArr {
+  int a_arr[10];
+};
+
 class ClassWithPtr {
   int *a_ptr;
 };
@@ -9,7 +13,9 @@ class ArrayDestructure {
     int a = 0;
     int &a_ref = a; // expected-note {{'a_ref' declared here}}
     int *a_ptr = &a; // expected-note {{'a_ptr' declared here}}
-    ClassWithPtr a_class; // expected-note {{'a_class' declared here}}
+
+    ClassWithArr a_arr_class;
+    ClassWithPtr a_ptr_class; // expected-note {{'a_class' declared here}}
     -> __fragment struct {
       constexpr auto val_a() {
         return a;
@@ -23,8 +29,12 @@ class ArrayDestructure {
         return a_ptr; // expected-error {{reference to local variable 'a_ptr' declared in enclosing function 'ArrayDestructure::__constexpr_decl'}}
       }
 
-      constexpr auto val_a_class() {
-        return a_class; // expected-error {{reference to local variable 'a_class' declared in enclosing function 'ArrayDestructure::__constexpr_decl'}}
+      constexpr auto val_a_arr_class() {
+        return a_arr_class;
+      }
+
+      constexpr auto val_a_ptr_class() {
+        return a_ptr_class; // expected-error {{reference to local variable 'a_class' declared in enclosing function 'ArrayDestructure::__constexpr_decl'}}
       }
     };
   }
