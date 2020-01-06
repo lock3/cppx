49e473459dd8d44db4f6b0af58d307114d609ddd
Andrew Sutton <asutton@lock3software.com>
2020-01-06 11:55:53 -0500

    Fixing file comment in BlueFrontend.cpp.

--- clang/lib/Blue/BlueFrontend.cpp
+++ clang/lib/Blue/BlueFrontend.cpp
@@ -1,4 +1,4 @@
-//===- BlueFrontend.cpp - Blue Frontend Action Interface ----------------===//
+//===- BlueFrontend.cpp - Blue Frontend Action Implementation -------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
