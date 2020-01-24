eb840e9167bcd3948c0fa7f4cebdbec76591e547
Andrew Sutton <asutton@lock3software.com>
2020-01-24 09:27:40 -0500

    Add missing file.

---
+++ clang/lib/Blue/BlueScope.cpp
@@ -0,0 +1,14 @@
+//===- BlueElaborator.cpp - Scope Constructs ------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (c) Lock3 Software 2019, all rights reserved.
+//
+//===----------------------------------------------------------------------===//
+//
+//  Definition of scope facilities.
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/Blue/BlueScope.h"
