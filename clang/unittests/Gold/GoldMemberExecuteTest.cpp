6c054b8a8a4f16e4ceb7a7e1bd3d45b2af29cbbc
bbartman-ilambda <bbartman@ilambda.ai>
2020-03-12 08:26:33 -0400

    Added another compilation test.

--- clang/unittests/Gold/GoldMemberExecuteTest.cpp
+++ clang/unittests/Gold/GoldMemberExecuteTest.cpp
@@ -24,3 +24,20 @@ main() : int!
   int result = CB();
   ASSERT_EQ(result, 4);
 }
+
+TEST(GoldClassExec, SimpleComparisonTest) {
+  StringRef Code = R"(
+x : int = 5
+y = 32
+main() : int!
+  return y > x
+)";
+  llvm::LLVMContext Context;
+  std::unique_ptr<ExecutionEngine> EE;
+  ASSERT_TRUE(CompileGoldCode(Context, Code, EE));
+  using MainSig = int(*)();
+  MainSig CB = (MainSig)EE->getPointerToNamedFunction("main");
+  ASSERT_TRUE(CB);
+  int result = CB();
+  ASSERT_EQ(result, 1);
+}
