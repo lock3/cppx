c3d3689a719439324c09a5ee30278cc6310cf702
bbartman-ilambda <bbartman@ilambda.ai>
2020-03-30 07:04:07 -0400

    Working on implementing class templates.

---
+++ clang/unittests/Gold/ClassTemplateParsing.cpp
@@ -0,0 +1,36 @@
+//=== ClassTempalteParsing.cpp - Elaboration for Gold Nodes ----------------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (c) Lock3 Software 2019, all rights reserved.
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file implements some of the tests for the gold language parser.
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "ParseUtil.h"
+#include "ASTMatchersTest.h"
+
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+using namespace clang;
+using namespace gold;
+
+TEST(ClassTemplate, ClassTemplateDeclaration) {
+StringRef Code = R"(
+c[x:type] : type = class:
+  z : int
+  y : bool
+
+main() : int!
+  temp : c[int]
+  return 0
+)";
+  DeclarationMatcher ClassC = recordDecl();
+  ASSERT_TRUE(matches(Code, ClassC));
+}
+
