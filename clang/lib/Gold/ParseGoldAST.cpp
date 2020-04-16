1828d36300ded7443bc6785061e1f1d75869b341
bbartman-ilambda <bbartman@ilambda.ai>
2020-04-16 10:32:15 -0400

    Removed logging message.

--- clang/lib/Gold/ParseGoldAST.cpp
+++ clang/lib/Gold/ParseGoldAST.cpp
@@ -60,7 +60,6 @@ void ParseGoldAST(clang::ASTContext &ClangContext, clang::Preprocessor &PP,
     if (D && !Consumer->HandleTopLevelDecl(DPtr.get()))
       return;
   }
-  TU->dump();
   Consumer->HandleTranslationUnit(ClangSema.getASTContext());
 }
 
