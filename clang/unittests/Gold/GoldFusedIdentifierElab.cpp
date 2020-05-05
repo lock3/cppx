7050e313815196a78a4a991980da30063a30b9b5
bbartman-ilambda <bbartman@ilambda.ai>
2020-05-05 12:55:26 -0400

    Finished testing basic elaboration of fused identifiers.

--- clang/unittests/Gold/GoldFusedIdentifierElab.cpp
+++ clang/unittests/Gold/GoldFusedIdentifierElab.cpp
@@ -1,31 +1,35 @@
-//===- unittest/Gold/GoldFusedIdentifierParse.cpp -------------------------===//
+//===- unittest/Gold/GoldFusedIdentifierElab.cpp --------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-
 #include "GoldParseUtil.h"
+#include "GoldASTMatchersTest.h"
+
 
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
 using namespace clang;
 using namespace gold;
 
-
-// These tests are just to make sure that we can process the code,
-// it doesn't matter if it has real meaning or not.
-TEST(GoldFusedIdentifer, DoubleQuoteFusedIdentifier) {
+TEST(GoldFusedIdentiferElab, DoubleQuoteFusedIdentifier) {
   StringRef Code = R"(
 x"x " : int
 )";
-  SimpleGoldParseTest(Code);
+  DeclarationMatcher FusedIdentifier = varDecl(
+    hasName("x\"x \"")
+  );
+  ASSERT_TRUE(matches(Code, FusedIdentifier));
 }
 
-TEST(GoldFusedIdentifer, SingleQuoteFusedIdentifier) {
+TEST(GoldFusedIdentiferElab, SingleQuoteFusedIdentifier) {
   StringRef Code = R"(
 x'x' : int
 )";
-  SimpleGoldParseTest(Code);
+  DeclarationMatcher FusedIdentifier = varDecl(
+    hasName("x'x'")
+  );
+  ASSERT_TRUE(matches(Code, FusedIdentifier));
 }
