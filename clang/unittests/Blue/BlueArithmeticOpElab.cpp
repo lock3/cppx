aa9c0376a0e3ed6e7a49a5c7d6bd8cca20172cb6
Brian Bartman <bbartman@kent.edu>
2021-01-06 11:24:07 -0500

    Added operator tests.

---
+++ clang/unittests/Blue/BlueArithmeticOpElab.cpp
@@ -0,0 +1,76 @@
+//=== BlueArithmeticOpElab.cpp --------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (c) Lock3 Software 2019, all rights reserved.
+//
+//===----------------------------------------------------------------------===//
+//
+//  Testing arithmetic operators.
+//
+//===----------------------------------------------------------------------===//
+#include "BlueParseUtil.h"
+#include "BlueASTMatchersTest.h"
+
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+using namespace clang;
+using namespace blue;
+
+TEST(BlueArithmeticOp, Plus) {
+  StringRef Code = R"BLUE(
+4 + 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("+"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueArithmeticOp, Minus) {
+  StringRef Code = R"BLUE(
+4 + 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("-"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueArithmeticOp, Multiplication) {
+  StringRef Code = R"BLUE(
+4 * 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("*"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueArithmeticOp, Division) {
+  StringRef Code = R"BLUE(
+4 / 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("*"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueArithmeticOp, Modulus) {
+  StringRef Code = R"BLUE(
+4 % 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("*"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+
+TEST(BlueArithmeticOp, UnaryPlus) {
+  StringRef Code = R"BLUE(
++5
+  )BLUE";
+  auto ToMatch = unaryOperator(hasOperatorName("+"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueArithmeticOp, UnaryMinus) {
+  StringRef Code = R"BLUE(
+-5
+  )BLUE";
+  auto ToMatch = unaryOperator(hasOperatorName("-s"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
---
+++ clang/unittests/Blue/BlueBitwiseOpElab.cpp
@@ -0,0 +1,67 @@
+//=== BlueBitwiseOpElab.cpp -----------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (c) Lock3 Software 2019, all rights reserved.
+//
+//===----------------------------------------------------------------------===//
+//
+//  Testing all of the bitwise operators.
+//
+//===----------------------------------------------------------------------===//
+#include "BlueParseUtil.h"
+#include "BlueASTMatchersTest.h"
+
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+using namespace clang;
+using namespace blue;
+
+TEST(BlueBitwiseOp, BWAnd) {
+  StringRef Code = R"BLUE(
+4 & 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("&"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueBitwiseOp, BWOr) {
+  StringRef Code = R"BLUE(
+4 | 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("|"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueBitwiseOp, BWXOr) {
+  StringRef Code = R"BLUE(
+4 ^ 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("^"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueBitwiseOp, LeftShift) {
+  StringRef Code = R"BLUE(
+4 << 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName("<<"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueBitwiseOp, BWRightShift) {
+  StringRef Code = R"BLUE(
+4 >> 5
+  )BLUE";
+  auto ToMatch = binaryOperator(hasOperatorName(">>"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
+
+TEST(BlueBitwiseOp, BWComposite) {
+  StringRef Code = R"BLUE(
+~4
+  )BLUE";
+  auto ToMatch = unaryOperator(hasOperatorName("~"));
+  ASSERT_TRUE(matches(Code.str(), ToMatch));
+}
